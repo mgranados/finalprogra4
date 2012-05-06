a0ec8b277ea4829da5011254ce495e4f42653433
abrgnz <crazymarines2@gmail.com>
2012-05-06 02:49:03 -0500

    hola

--- clienteservidor/server.c
+++ clienteservidor/server.c
@@ -7,6 +7,13 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+     
+     char buffer[512];
+
 
 void error(const char *msg)
 {
@@ -14,13 +21,26 @@ void error(const char *msg)
     exit(1);
 }
 
+void *mostrardir()
+{
+   printf("%s",getcwd(buffer,-1));
+}
+
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
+     int sockfd,newsockfd, portno;
      socklen_t clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
      int n;
+     char *cmd;
+     char *atb1;
+     char *atb2;
+     int caso,i;
+     
+     //Creacion de Threads
+     int cic=0; //Variable de cantidad de threads
+     pthread_t tid[5000];//Cantidad de threads -- Request al servidor
+     struct sockaddr_in serv_addr, cli_addr;
+    
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
@@ -47,11 +67,60 @@ int main(int argc, char *argv[])
      bzero(buffer,256);
      n = recv(newsockfd,buffer,255,0);
      if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
+     
+     printf("%s",buffer);
+     //Variables para recibir el string del cliente
+     cmd=strtok(buffer, " ");
+     atb1= strtok(NULL, " ");
+     atb2= strtok(NULL, " ");
+     
+     if(strcmp(cmd,"pwd")==0)
+         caso=1;
+     else if(strcmp(cmd,"ls")==0)
+         caso=2;
+     else if(strcmp(cmd,"cd")==0)
+         caso=3;
+     else if(strcmp(cmd,"mkdir")==0)
+         caso=4;
+     else if(strcmp(cmd,"rmdir")==0)
+         caso=5;
+     else if(strcmp(cmd,"vi")==0)
+         caso=6;
+     else if(strcmp(cmd,"cat")==0)
+         caso=7;
+     else if(strcmp(cmd,"cp")==0)
+         caso=8;
+     else if(strcmp(cmd,"mv")==0)
+         caso=9;
+     else if(strcmp(cmd,"rm")==0)
+         caso=10;
+     else if(strcmp(cmd,"exit")==0){
+         n = write(newsockfd,"exit",18);
+         //enviar mensaje de que se acabo
+         
+     }
+     
+     switch(caso){
+         
+    case 1:
+    cic++; 
+         n = write(newsockfd,"Entro al caso 1",18);
+    pthread_create(&tid[cic],NULL, &mostrardir,NULL);
+    
+         
+     }
+    
+    for(i=0;i<cic;i++)
+    pthread_join(tid[i],NULL);
+     
+     
+     
+     
+
      close(newsockfd);
      //close(sockfd);
  }
      return 0; 
 }
+
+
